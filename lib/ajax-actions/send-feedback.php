03d2b8c1f78ec442ee76198e3f8b8c3a4385b11b
emandiev <emandiev@gmail.com>
2021-08-08 17:36:03 +0300

    Change feedback form email recipient

--- lib/ajax-actions/send-feedback.php
+++ lib/ajax-actions/send-feedback.php
@@ -15,11 +15,11 @@ add_action( 'wp_ajax_woo_image_seo_send_feedback', function() {
 
     wp_send_json(
         wp_mail(
-            'danail@emandiev.com',
+            'emandiev@gmail.com',
             'Woo Image SEO Plugin Feedback',
             'Email: ' . esc_html( $_POST['email'] ) . '<br>Message: ' . esc_html( $_POST['message'] ),
             [
-                'From: Woo Image SEO <danail@emandiev.com>',
+                'From: Woo Image SEO <emandiev@gmail.com>',
                 'Content-Type: text/html; charset=UTF-8',
             ]
         )
