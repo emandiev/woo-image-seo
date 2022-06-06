d614d3859306ffee5a8c44b98ba329dc69122748
emandiev <emandiev@gmail.com>
2022-06-06 10:01:56 +0300

    Skip images with the 'woo-image-seo-skip' class

--- lib/hooks/public.php
+++ lib/hooks/public.php
@@ -3,12 +3,17 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 /**
- * Add our custom filter to the attributes of WooCommerce products
+ * Add our custom filter to the image attributes
  */
 add_filter( 'wp_get_attachment_image_attributes', function( $attr ) {
     if ( get_post_type() !== 'product' ) {
         return $attr;
     }
 
+    // skip images with the "woo-image-seo-skip" class
+    if ( ! empty( $attr['class'] ) && strpos( $attr['class'], 'woo-image-seo-skip' ) !== false ) {
+        return $attr;
+    }
+
     return woo_image_seo_get_image_attributes( $attr );
-}, PHP_INT_MAX, 2 );
+}, PHP_INT_MAX );
