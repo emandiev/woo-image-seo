6fe6bb7c41b654cc5efc8886cb86db75954ac0a2
emandiev <emandiev@gmail.com>
2022-07-22 20:27:15 +0300

    Improve ad banner sizing

--- views/partials/postbox/ad.php
+++ views/partials/postbox/ad.php
@@ -8,7 +8,7 @@ All purchases made from this link will generate an affiliate bonus for us. Thank
     <img
         src="<?php echo WOO_IMAGE_SEO['assets_url'] . 'siteground-ad-2x.png?version=' . WOO_IMAGE_SEO['version'] ?>"
         alt=""
-        width="317"
+        width="315"
         height="200"
         style="display: block;margin-top: 5px;border: 1px solid #c3c4c7;"
     >
