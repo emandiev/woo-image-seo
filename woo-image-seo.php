3edd04031f7bb44f273b54ef4ee5860ca0330aac
emandiev <emandiev@gmail.com>
2023-12-02 17:12:37 +0200

    Bump WC tested up to

--- woo-image-seo.php
+++ woo-image-seo.php
@@ -13,7 +13,7 @@
 	Requires at least: 4.1
 	Tested up to: 6.4.1
 	WC requires at least: 4.0
-	WC tested up to: 8.3.0
+	WC tested up to: 8.3.1
 */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
