cd4a9f1f9c23be34932df3172b744ee61f276cc2
emandiev <emandiev@gmail.com>
2024-08-18 11:27:03 +0300

    Add support for woo-variation-gallery plugin

--- lib/hooks/public.php
+++ lib/hooks/public.php
@@ -10,3 +10,14 @@ add_filter(
     'woo_image_seo_get_image_attributes',
     PHP_INT_MAX
 );
+
+/**
+ * Filter woo_variation_gallery_get_image_props to add SEO attributes
+ * This is used by the Woo Variation Gallery plugin
+ */
+add_filter(
+	'woo_variation_gallery_get_image_props',
+	'woo_image_seo_get_variation_gallery_image_attributes',
+	PHP_INT_MAX,
+	3
+);
