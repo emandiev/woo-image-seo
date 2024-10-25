c4d87925272fb0f022e401be604e82ab57b550d8
emandiev <emandiev@gmail.com>
2024-10-25 21:11:46 +0300

    Add pro version ad

---
+++ views/partials/pro-banner.php
@@ -0,0 +1,9 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+?>
+
+<div class="woo-image-seo-ad">
+    Woo Image SEO Pro is available!<br>Advanced features, premium support.<br>👉 <a href="https://wooimageseo.com/pro" target="_blank">https://wooimageseo.com/pro</a>
+</div>
