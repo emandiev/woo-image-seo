556c0bc2437ea4e05471f7d6523f1f3a6559daa9
emandiev <emandiev@gmail.com>
2025-01-06 20:45:54 +0200

    Update link

--- views/partials/pro-banner.php
+++ views/partials/pro-banner.php
@@ -5,5 +5,5 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
 
 <div class="woo-image-seo-ad">
-    Woo Image SEO Pro is available!<br>Advanced features, premium support.<br>👉 <a href="https://wooimageseo.com/pro" target="_blank">https://wooimageseo.com/pro</a>
+    Woo Image SEO Pro is available!<br>Advanced features, premium support.<br>👉 <a href="https://wooimageseo.com/#pro-features" target="_blank">Click here to learn more</a>
 </div>
